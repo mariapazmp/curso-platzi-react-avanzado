9115172b83d552bf8d70c6018e45979df5f45167
MariaPazMP <libelulaby@gmail.com>
2021-08-15 22:41:23 -0500

    Adding and using Styled Components

---
+++ src/components/category/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Anchor, Image} from "./styles";
+
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1548802673-380ab8ebc7b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=150&q=80';
+
+export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
+  <Anchor href={path}>
+      <Image src={cover} />
+      {emoji}
+  </Anchor>
+)
