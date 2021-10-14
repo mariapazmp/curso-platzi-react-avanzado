57bf27d0ddfd9ebeb896844704cdd03258f68860
MariaPazMP <libelulaby@gmail.com>
2021-10-14 22:42:36 +0200

    Adding hooks and styles!

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import { Category } from "../category";
-import { categories } from '../../../api/db.json'
 import { List, Item } from "./styles";
 
 export const ListOfCategories = () => {
-  return(
-    <List>
+  const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(function () {
+    window.fetch('https://petgram-server-24iykciv5.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, []);
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    }
+
+    document.addEventListener('scroll', onScroll);
+    return () => document.removeEventListener('scroll', onScroll);
+  })
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}>
           <Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return(
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }
