9c65fe3a4300c66a24cc306e87d2cc70241d0deb
MariaPazMP <libelulaby@gmail.com>
2021-10-18 22:10:23 +0200

    Patrón Render Props y enrutamiento a Detail

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { ListOfCategories } from "./components/ListOfCategories";
 import { GlobalStyles } from "./styles/GlobalStyles";
-import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
+import { ListOfPhotoCards } from "./container/ListOfPhotoCards";
 import { Logo } from "./components/Logo"
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-export const App = () => (
-  <>
-    <GlobalStyles />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards/>
-  </>
-);
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search);
+  const detailId = urlParams.get('detail');
+
+  return(
+    <>
+      <GlobalStyles />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <Fragment>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={1} />
+          </Fragment>
+      }
+    </>
+  )
+};
