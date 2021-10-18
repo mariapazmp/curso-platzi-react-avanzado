e2f5a9b6fce4f672d9b1a1afbb626157a3faf518
MariaPazMP <libelulaby@gmail.com>
2021-10-18 11:47:18 +0200

    Refactoring custom hooks

---
+++ src/hooks/useNearScreen.js
@@ -0,0 +1,20 @@
+import { useEffect, useState, useRef } from 'react'
+
+export function useNearScreen () {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    const observer = new window.IntersectionObserver(function (entries) {
+      const { isIntersecting } = entries[0]
+      console.log(isIntersecting)
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
+
+  return [show, element]
+}
